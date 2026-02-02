aabfe76a47c97c6ed5caa49abcd416d82f078c79
ZainDar12 <k250146@nu.edu.pk>
2026-02-02 22:59:05 +0500

    Update Q5.cpp
    Refactor Employee class to separate declaration and implementation. Added methods for getting data, calculating salary after tax, and displaying employee details.

--- Q5.cpp
+++ Q5.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
+#include "Employee.h"
 using namespace std;
-class Employee{
-    string name;
-    double salary;
-    int tax;
-public:
-    void SetTax(int t){
-        tax = t;
-       cout<<"Salary after tax: " << GetSalaryAfterTax() << endl;
-    }
-    double GetSalaryAfterTax(){
-        return salary - (salary * tax / 100);
-    }
-};
+void Employee::GetData(){
+    cout << "Enter Name: ";
+    cin >> name;
+    cout << "Enter Salary: ";
+    cin >> salary;
+}
+double Employee::SalaryAfterTax(){
+    return salary - (salary * tax / 100);
+}
+void Employee::SetTax(int t){
+    tax = t;
+}
+void Employee::Display(){
+    cout << "Name: " << name << endl;
+    cout << "Salary after tax: " << SalaryAfterTax() << endl;
+}
+int main(){
+    Employee e1,e2,e3;
+    cout<<"Enter details for Employee 1:"<<endl;
+    e1.GetData();
+    e1.SetTax(10); // Setting tax to 10%
+    cout<<"Enter details for Employee 2:"<<endl;
+    e2.GetData();
+    e2.SetTax(15); // Setting tax to 15%
+    cout<<"Enter details for Employee 3:"<<endl;
+    e3.GetData();
+    e3.SetTax(20); 
+    // Setting tax to 20%
+    return 0;
+}
